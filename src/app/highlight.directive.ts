0cfd0532136a74762d438cceb926bd6425470936
margaret mwangi <margaret13mwangi@gmail.com>
2020-03-29 19:30:21 +0300

    updated directives

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -1,10 +1,18 @@
-import { Directive } from '@angular/core';
+import { Directive ,ElementRef, Input, HostListener} from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]'
 })
 export class HighlightDirective {
+  constructor(private elem: ElementRef) { }
 
-  constructor() { }
-
+  @HostListener("click") upVotes() {
+    this.highlight("highlight")
+  }
+  @HostListener("dblclick") onDoubleClicks() {
+    this.highlight("highlight")
+  }
+  private highlight(highlight: string){
+    this.elem.nativeElement.style.backgroundColor = 'yellow';//style will be fill with yellow as bg color on click trigger
+  }
 }
