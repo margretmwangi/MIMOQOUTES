3072b389b2a8cf7d05c1f9a9155aa06d35bf22bf
margaret mwangi <margaret13mwangi@gmail.com>
2020-03-29 19:19:04 +0300

    vote.ts

--- src/app/vote/vote.component.ts
+++ src/app/vote/vote.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit , Output, EventEmitter} from '@angular/core';
+import {Quote} from '../quote'
 
 @Component({
   selector: 'app-vote',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VoteComponent implements OnInit {
 
+  @Output() isComplete = new EventEmitter<boolean>();//listens to button click to display results
+//starting point for votes in two variables with 0 as default
+    uvotes = 0;
+    dvotes = 0;
+
+    upVote(){
+      this.uvotes = this.uvotes + 1;//adds one on mouse click
+    }
+
+    downVote(){
+      this.dvotes = this.dvotes + 1;//adds one per mouse click
+    }
+
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
   }
 
 }
