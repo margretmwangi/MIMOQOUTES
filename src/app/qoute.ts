1a05e252123906f3a3aef7a6c1a5252f80d88acc
margaret mwangi <margaret13mwangi@gmail.com>
2020-03-29 19:32:28 +0300

    new updates

--- src/app/qoute.ts
+++ src/app/qoute.ts
@@ -1,2 +1,14 @@
 export class Qoute {
+    
+showDescription= false;
+
+constructor(
+  public id: number,
+  public name: string,
+  public description: string,
+  public submitter: string,
+  public completeDate: Date) {
 }
+}
+
+//contains properties of all inputs
